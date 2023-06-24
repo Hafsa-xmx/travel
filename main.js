5011eb96ea44f66f12500142c43712502b630300
Hafsa-xmx <hafsamahamuud6@gmail.com>
2023-06-24 09:31:48 -0700

    this is my project

---
+++ main.js
@@ -0,0 +1,11 @@
+const toggle_btn =document.querySelector(".toggle_btn");
+const links =document.querySelector(".links");
+
+
+toggle_btn.addEventListener("click" ,() => {
+    links.classList.toggle("active");
+
+})
+
+
+
